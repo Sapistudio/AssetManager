8804a43e6ee8423cf8167b3a6f412a64d9fd949d
laur sandu <Sapistudio@users.noreply.github.com>
2020-05-06 20:39:42 +0300

    Update AssetInstaller.php

--- src/AssetInstaller.php
+++ src/AssetInstaller.php
@@ -1,81 +1,83 @@
 <?php
 namespace Sapistudio\AssetManager;
 
+use Composer\Composer;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+use Composer\Script\ScriptEvents;
+use Composer\Installer\PackageEvent;
+use Composer\Script\CommandEvent;
+use Composer\Util\Filesystem;
+use Composer\Package\BasePackage;
+
+
 use Composer\Installer\LibraryInstaller;
 use Composer\Package\PackageInterface;
 
+
 class AssetInstaller extends LibraryInstaller
 {
-    protected $configuration;
-
+    const DEFAULT_ASSET_FOLDER  = 'vendorAssets';
+    const EXTRA_ASSETS_PATH     = 'assetsPath';
+    protected $assetConfiguration;
+    protected $assetsFolderPath = null;
+    protected $configComposer;
+    protected $filesystem;
+    protected $rules;
+    
+    public function __construct(IOInterface $io,Composer $composer){
+        $this->filesystem       = new Filesystem();
+        $extraConfig            = $composer->getPackage()->getExtra();
+        $this->assetsFolderPath = (isset($extraConfig[self::EXTRA_ASSETS_PATH])) ? $extraConfig[self::EXTRA_ASSETS_PATH] : realpath(getcwd()).DIRECTORY_SEPARATOR.self::DEFAULT_ASSET_FOLDER;
+        $this->filesystem->ensureDirectoryExists($this->assetsFolderPath);
+        parent::__construct($io,$composer);
+    }
+    
     public function getInstallPath(PackageInterface $package)
     {
-        $configuration = $this->getPluginConfiguration();
-        $pattern = $configuration->getPattern($package);
+        $pattern = $this->getPluginConfiguration()->getPattern($package);
         if ($pattern) {
-            $basePath = $this->buildPath($pattern, $this->getPackageReplacementTokens($package));
-            $targetDir = $package->getTargetDir();
-            return $basePath . ($targetDir ? '/'.$targetDir : '');
+            $basePath   = $this->buildPath(rtrim($this->assetsFolderPath,DIRECTORY_SEPARATOR).DIRECTORY_SEPARATOR.ltrim($pattern,DIRECTORY_SEPARATOR), $this->getPackageReplacementTokens($package));
+            $targetDir  = $package->getTargetDir();
+            $dsads = $basePath . ($targetDir ? '/'.$targetDir : '');
         } else {
-            return parent::getInstallPath($package);
+            $dsads =  parent::getInstallPath($package);
         }
+        
+        
+        print_R($dsads);
+        die();
+        return parent::getInstallPath($package);
     }
 
     public function supports($packageType)
     {
-        // The installer may support any package type, but we allways skip some composer specific types with special handling.
-        if (in_array($packageType, array('metapackage', 'composer-plugin'))) {
-            return false;
-        }
         return $this->getPluginConfiguration()->isPackageTypeSupported($packageType);
     }
 
     protected function getPackageReplacementTokens(PackageInterface $package)
     {
-        $vars = array(
-          '{$type}' => $package->getType(),
-        );
-
+        $vars       = ['{$type}' => $package->getType()];
         $prettyName = $package->getPrettyName();
-        if (strpos($prettyName, '/') !== false) {
-            $pieces = explode('/', $prettyName);
-            $vars['{$vendor}'] = $pieces[0];
-            $vars['{$name}'] = $pieces[1];
-
-        } else {
-            $vars['{$vendor}'] = '';
-            $vars['{$name}'] = $prettyName;
+        $pieces     = ['{$vendor}' => '','{$name}' => $prettyName];
+        if (strpos($prettyName, '/') !== false){
+            $pieces = array_combine(['{$vendor}','{$name}'],explode('/', $prettyName,2));
         }
-
-        return $vars;
+        return array_merge($vars, $pieces);
     }
 
-    protected function buildPath($pattern, array $tokens = array())
+    protected function buildPath($pattern, $tokens = [])
     {
         return strtr($pattern, $tokens);
     }
 
     protected function getPluginConfiguration()
     {
-        if (!isset($this->configuration)) {
+        if (!isset($this->assetConfiguration)) {
             $extra = $this->composer->getPackage()->getExtra();
-
-            // We check if we need to support the legacy configuration.
-            $legacy = false;
-            if (isset($extra['custom-installer'])) {
-                // Legacy
-                $legacy = true;
-                foreach ($extra['custom-installer'] as $key => $val) {
-                    if (is_array($val)) {
-                        $legacy = false;
-                        break;
-                    }
-                }
-            }
-
-            $this->configuration = new AssetConfiguration($extra);
+            $this->assetConfiguration = new AssetConfiguration($extra);
         }
-
-        return $this->configuration;
+        return $this->assetConfiguration;
     }
 }
